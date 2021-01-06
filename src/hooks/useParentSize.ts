7688a762fc77b4e6fdea2bf959f5f583ad6ba6bd
TanyaIgnatenko <tanigna.work@gmail.com>
2021-01-06 20:18:09 +0300

    TypeScript fixes

--- src/hooks/useParentSize.ts
+++ src/hooks/useParentSize.ts
@@ -1,12 +1,16 @@
 import { useState, useEffect, RefObject } from "react";
 
-export const useParentSize = (nodeRef: RefObject<HTMLElement>) => {
-  const [parentSize, setParentSize] = useState({});
+type Size = {width: number, height: number}
+type UseParentSize = (_: RefObject<HTMLElement>) => Size | undefined;
+
+export const useParentSize: UseParentSize = (nodeRef) => {
+  const [parentSize, setParentSize] = useState<Size>();
 
   useEffect(() => {
     if (!nodeRef.current) return;
 
-    const { width, height } = nodeRef.current.parentNode.getBoundingClientRect();
+    const parentNode = nodeRef.current.parentNode as HTMLElement;
+    const { width, height } = parentNode.getBoundingClientRect();
 
     setParentSize({ width, height });
   }, [nodeRef]);
--- src/withDefaultRelativePosition.tsx
+++ src/withDefaultRelativePosition.tsx
@@ -1,29 +1,46 @@
-// @ts-nocheck
-import React, { useRef } from "react";
+import React, { ComponentType, ReactNode, useRef } from "react";
 import { useParentSize } from "./hooks/useParentSize";
+import { Props } from "../lib";
 
-export const withDefaultRelativePosition = (WrappedComponent) => {
+type ExtendedDefault = {
+  x?: number;
+  y?: number;
+  left?: number;
+  right?: number;
+  top?: number;
+  bottom?: number;
+} & {
+  width: number;
+  height: number;
+};
+type ReturnedComponent = (_: Omit<Props, "default"> & {default: ExtendedDefault}) => ReactNode;
+type WithDefaultRelativePosition = (_: ComponentType<Props>) => ReturnedComponent;
+
+export const withDefaultRelativePosition: WithDefaultRelativePosition = (WrappedComponent) => {
   return ({ default: defaultValue, ...restProps }) => {
    const wrapperNodeRef = useRef(null);
     const parentSize = useParentSize(wrapperNodeRef);
 
-    if (!defaultValue) return <WrappedComponent {...restProps} />;
+    if (!defaultValue) {
+      return <WrappedComponent {...restProps} />;
+    }
 
     const needsWindowSize = defaultValue.right || defaultValue.bottom;
-    const parentSizeIsAbsent = !parentSize.width || !parentSize.height;
 
-    if (needsWindowSize && parentSizeIsAbsent) {
+    if (needsWindowSize && !parentSize) {
       return (
-        <div ref={wrapperNodeRef} style={{ height: "100%" }}/>
+        <div ref={wrapperNodeRef} style={{ height: "100%" }}>
+          <WrappedComponent {...restProps} default={defaultValue} style={{ visible: false }}/>
+        </div>
       );
     }
 
     const processedDefaultX = defaultValue.x !== undefined
       ? defaultValue.x
-      : defaultValue.left || (parentSize.width - (defaultValue.width + defaultValue.right));
+      : defaultValue.left || (parentSize!.width - (defaultValue.width + defaultValue.right));
     const processedDefaultY = defaultValue.y !== undefined
       ? defaultValue.y
-      : defaultValue.top || (parentSize.height - (defaultValue.height + defaultValue.bottom));
+      : defaultValue.top || (parentSize!.height - (defaultValue.height + defaultValue.bottom));
 
     const processedDefault = {
       ...defaultValue,
